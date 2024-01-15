fa8dcabc393bab0adf4c21b4554621db732b62f7
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2024-01-15 16:49:54 -0300

    <refactor>debugs_ga_gtm

--- assets/js/gtm_head.js
+++ assets/js/gtm_head.js
@@ -5,12 +5,3 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-5229WJTG');</script>
     <!-- End Google Tag Manager -->
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YWC9WKTQ23"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-YWC9WKTQ23');
-    </script>
