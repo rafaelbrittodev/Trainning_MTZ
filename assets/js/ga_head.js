0a0a043357cbf341832ed4ee42dfbc8537038617
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2024-01-17 15:39:25 -0300

    <update>otimeze_gtm

--- assets/js/ga_head.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YWC9WKTQ23"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-YWC9WKTQ23');
-    </script>
